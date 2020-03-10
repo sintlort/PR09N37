6ed6c5fed5fc860ad952c6a8c090828137d0b07b
sintlort <61153093+sintlort@users.noreply.github.com>
2020-03-10 21:05:51 +0800

    0.1

---
+++ database/seeds/AdminTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'name' => 'admin',
+            'email' => 'admin@itunud.com',
+            'password' => bcrypt('123456'),
+        ]);
+    }
+}
